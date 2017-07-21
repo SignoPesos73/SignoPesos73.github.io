2dfd818d94050f5fde46b35a248a456a18b048b8
mathdecision <hsebasa@gmail.com>
2017-07-21 10:33:01 -0500

    s

--- app/build/resources/server_utils.js
+++ app/build/resources/server_utils.js
@@ -1,71 +1,77 @@
 angular.module('server.utils', [])
 
-.factory('main_server', ['$http', function($http){
-    var serverAddress = 'https://transformers-satrack.appspot.com/';
-    var serverPort = '';
-    var fullServerAddress = serverAddress + ':' + serverPort + '/';
+    .factory('main_server', ['$http', function($http){
+        var serverAddress = 'https://transformers-satrack.appspot.com/';
+        var serverPort = '';
+        var fullServerAddress;
 
-    function sendMessage(msg, method, headers, progressHandler){
-        return $http({
-            url: fullServerAddress,
-            method: method ? method : 'POST',
-            data: msg,
-            headers: headers,
-            withCredentials: false,
-            crossDomain: true,
-            eventHandlers: {
-                progress: progressHandler ? progressHandler: function(){}
-            }
-        });
-    }
+        if (serverPort != ''){
+            fullServerAddress = serverAddress + ':' + serverPort + '/';
+        } else {
+            fullServerAddress = serverAddress;
+        }
 
-    function getPortfolios(){
-        return $http({
-            url: fullServerAddress+'getPortfolios',
-            method: 'GET',
-            withCredentials: false,
-            crossDomain: true
-        });
-    }
+        function sendMessage(msg, method, headers, progressHandler){
+            return $http({
+                url: fullServerAddress,
+                method: method ? method : 'POST',
+                data: msg,
+                headers: headers,
+                withCredentials: false,
+                crossDomain: true,
+                eventHandlers: {
+                    progress: progressHandler ? progressHandler: function(){}
+                }
+            });
+        }
 
-    function testSend(msg){
-        return {
-            then: function(f){
-                f(3);
+        function getPortfolios(){
+            return $http({
+                url: fullServerAddress+'getPortfolios',
+                method: 'GET',
+                withCredentials: false,
+                crossDomain: true
+            });
+        }
+
+        function testSend(msg){
+            return {
+                then: function(f){
+                    f(3);
+                }
             }
         }
-    }
 
-    return {
-        serverAddress: serverAddress,
-        serverPort: serverPort,
-        sendMessage: sendMessage,
-        sendPersonalInfo: function(infoJson){
-            return sendMessage(infoJson, 'POST');
-        },
-        getPortfolios: getPortfolios
-    }
-}])
-.factory('user_data', [function(){
-    return {
-        data: {
-            nombre: {
-                text: ''
+        return {
+            serverAddress: serverAddress,
+            serverPort: serverPort,
+            sendMessage: sendMessage,
+            sendPersonalInfo: function(infoJson){
+                return sendMessage(infoJson, 'POST');
             },
-            apellidos: {
-                text: ''
+            getPortfolios: getPortfolios
+        }
+    }])
+    .factory('user_data', [function(){
+        return {
+            data: {
+                nombre: {
+                    text: ''
+                },
+                apellidos: {
+                    text: ''
+                },
+                cedula: {
+                    text: ''
+                },
+                administradora: {
+                    text: ''
+                },
+                nombre_fondo: {
+                    text: ''
+                }
             },
-            cedula: {
-                text: ''
-            },
-            administradora: {
-                text: ''
-            },
-            nombre_fondo: {
-                text: ''
-            }
-        },
-        results: null
-    }
-}])
+            results: null
+        }
+    }])
 ;
